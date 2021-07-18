fdcbf6e3f8d2e14c8a77c581552314ea78eebc14
Thwin Soe <thwinaungsoe@yahoo.com>
2021-07-18 11:50:55 +1000

    2 new model classes checked-in: (1)OccupationRating + (2) RatingFactor.

---
+++ Models/OccupationRating.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevelopmentProject.Models
+{
+    public class OccupationRating
+    {
+        public int Id { get; set; }
+
+        [StringLength(100, MinimumLength = 5)]
+        [Required]
+        public string Occupation { get; set; }
+
+        [StringLength(100, MinimumLength = 5)]
+        [Required]
+        public string Rating { get; set; }
+    }
+}
---
+++ Models/RatingFactor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevelopmentProject.Models
+{
+    public class RatingFactor
+    {
+        public int Id { get; set; }
+
+        [StringLength(100, MinimumLength = 5)]
+        [Required]
+        public string Rating { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        [Required]
+        public decimal Factor { get; set; }
+    }
+}
